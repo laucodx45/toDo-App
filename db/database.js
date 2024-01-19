f7ac5073dac7b775fa604e931ea710ec14768edf
Cody <codylau6233@hotmail.com>
2024-01-19 15:12:10 -0800

    feat(backEndDatabase): Implement user authentication and connect to ElephantSQL
    - Created a new branch 'feat/backEndDatabase' for backend database integration
    - Implemented a new login route to serve the login page
    - Established a connection to ElephantSQL to store users and user data
    - Added a new 'database.js' file with functions to handle database interactions for user authentication

---
+++ db/database.js
@@ -0,0 +1,32 @@
+const { Client } = require('pg');
+const path = require('path');
+const PATH = path.resolve(__dirname, '../.env');
+require('dotenv').config(path, PATH);
+
+const client = new Client(process.env.URL);
+client.connect();
+
+const getUsername = (username) => {
+  const queryString = `SELECT username, password FROM users WHERE username = $1`;
+  const values = [username];
+  return client.query(queryString, values)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return "cannot find data";
+      } else {
+        return result.rows[0];
+      }
+      
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+
+// getUsername('cody').then((result) => {
+//   console.log(result);
+// });
+module.exports = {
+  getUsername
+};
--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const database = require('./db/database');
+// test username = cody
+// test pw = password
 
+///////////////////////////////////////////
+// middleware/////////////////////////////
 app.use(express.static('public'));
-app.listen(8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({ extended: false }));
+
+// app.get('/', (req, res) => {
+//   // once user logged in, we want to establish a cookie
+//   // if no cookie then we have to redirect user to /login
+// });
+const PATH = path.resolve(__dirname, './public/login.html');
+app.get('/login', (req, res) => {
+  res.sendFile(PATH);
+});
+
+app.post('/login', (req, res) => {
+  const inputUsername = req.body.username;
+  const inputPassword = req.body.password;
+  database
+    .getUsername(inputUsername).then((obj) => {
+      if (obj.username === inputUsername) {
+        bcrypt.compare(inputPassword, obj.password, function(err, result) {
+          if (result) {
+            // res.send('user is in our database!');
+            res.redirect('/');
+          }
+        });
+      }
+  })
+  // a function imported, to look up username and the hashed password from database
+  
+});
+
+
+app.listen(8080, () => {
+  console.log(`Server is running on 8080`);
+});
